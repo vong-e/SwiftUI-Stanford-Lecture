3626dd92abaf7d3bbabeda621c96a862579da2dd
vong-e <sinkim123321@gmail.com>
2022-03-03 04:19:26 +0900

    - Lecture 2 finished

--- MemorizeSample/ContentView.swift
+++ MemorizeSample/ContentView.swift
@@ -8,19 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🚲", "🛵", "🏍", "🛺", "🚔", "🚍", "🚘", "🚖", "🚝", "🚞"]
+    @State var emojiCount = 6
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
-            CardView(isFaceUp: false)
-            CardView(isFaceUp: true)
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self ) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
 struct CardView: View {
+    var content: String
     @State var isFaceUp: Bool = true
     
     var body: some View {
@@ -28,8 +62,8 @@ struct CardView: View {
             let shape  = RoundedRectangle(cornerRadius: 25)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
@@ -44,7 +78,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
-        ContentView()
-            .preferredColorScheme(.light)
+//        ContentView()
+//            .preferredColorScheme(.light)
     }
 }
